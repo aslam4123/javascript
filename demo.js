5ae530cd2ebb502197891fd9beb2918691e6729f
aslam <thoppilaslam0987654321@gmail.com>
2024-12-04 09:01:58 +0530

    new

---
+++ demo.js
@@ -0,0 +1,58 @@
+// var a=10
+// var a=20
+// alert(a)
+
+
+// a=10
+// a=30
+// console.log(a);
+
+// let a=10
+// a=20
+// console.log(a);
+
+
+// let a=10
+// let a="aslam"
+// let a=[1,2,3,['abc']]
+// let a={name:'aslam',age:21}
+// let a
+// let a=null
+// console.log(typeof a);
+// console.log(a);
+
+
+// console.log(10+5);
+// console.log(10-5);
+// console.log(10*5);
+// console.log(10/5);
+// console.log(10%3);
+// console.log(10**5);
+
+// a=10
+// a++
+// a--
+// ++a
+// --a
+// console.log(a);
+
+// a=10
+// a+=2
+// a-=2
+// a*=2
+// a/=2
+// a**=2
+// console.log(a);
+
+// console.log(10==10);
+// console.log(10=="10");
+// console.log(10==="10");
+
+// console.log(10!=11);
+// console.log(10!=="10");
+
+
+
+
+
+
